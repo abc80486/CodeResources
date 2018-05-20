c4197d17359ce954ecb3a6d7416bb9a3f933b96e
xiangbin <abc80486@163.com>
2018-05-20 14:53:25 +0800

    MacBook

--- DataStructure/第 6 章 树和二叉树/6.4 先序中序后序遍历二叉树.cpp
+++ DataStructure/第 6 章 树和二叉树/6.4 先序中序后序遍历二叉树.cpp
@@ -10,3 +10,18 @@ if(T){
                 return OK;
     return OK;
 }else return OK;
+}
+Status InOrderTravverse(BiTree T,Status(*Visit)(TElemType e)){
+    InitStack(S);
+    Push(S,T);
+    while(!StackEmpty(S)){
+        while(GetTop(S,p)&&p) Push(S,p->lchild);
+        Pop(S,p);
+        if(!StackEmpty(S)){
+            Pop(S,p);
+            if(!Visit(p->data)) return ERROR;
+            Push(S,p->rchild);
+        }
+    }
+    return OK;
+}
