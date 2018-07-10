a3b035ff3e2c091480cbaedd579ad5842f22e970
xiangbin <abc80486@163.com>
2018-07-10 13:43:57 +0800

    7.8

--- OnlineJudge/UVa2/unit 1/10881 Piotr's Ants/10881.cpp
+++ OnlineJudge/UVa2/unit 1/10881 Piotr's Ants/10881.cpp
@@ -46,8 +46,8 @@ int main(){
             else{
                 printf("%d %s\n",after[a].p,dirName[after[a].d+1]);
             }
-           // printf("\n");
-        }
+        } 
+        printf("\n");
     }
     return 0;
 }
