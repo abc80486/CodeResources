8168fceec899146abe5ab839276992d263c12b96
xiangbin <abc80486@163.com>
2018-08-10 12:34:40 +0800

    mac 8/10

--- OnlineJudge/OJ/PAT/PAT_B_15/1046.cpp
+++ OnlineJudge/OJ/PAT/PAT_B_15/1046.cpp
@@ -1,6 +1,15 @@
 //字符处理；
 #include<iostream>
 int main(){
-    
+    int a=0,b=0;
+    int n;
+    scanf("%d",&n);
+    while(n--){
+        int a1,a2,b1,b2;
+        scanf("%d %d %d %d",&a1,&a2,&b1,&b2);
+        if(a2==a1+b1&&b2!=a2) b++;
+        if(b2==a1+b1&&a2!=b2) a++;
+    }
+    printf("%d %d\n",a,b);
     return 0;
 }
